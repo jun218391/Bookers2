ae7e5d8cd8d2b2ab048c9674a0d3d68ab7b4f7af
jun218391 <122144759+jun218391@users.noreply.github.com>
2023-01-26 13:23:50 +0000

    [add]4-25

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only:[:new, :create, :index, :destroy, :show]
-  resources :users, only:[:show, :edit]
+  resources :users, only:[:show, :edit, :update]
 end
