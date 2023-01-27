77e10aaee34bb811b4828fea566f39b79a3dc6c1
jun218391 <122144759+jun218391@users.noreply.github.com>
2023-01-27 14:01:19 +0000

    [add]4-25

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   
-  # get 'books/new'
-  # get 'books/index'
-  # get 'books/show'
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only:[:new, :create, :index, :destroy, :show]
